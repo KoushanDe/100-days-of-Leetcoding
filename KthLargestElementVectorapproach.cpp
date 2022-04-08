ce1467d91dcf7326176bac200c7beb612c165b2d
KoushanDe <koushanriki007@gmail.com>
2022-04-08 22:58:35 +0530

    Day 11

---
+++ KthLargestElementVectorapproach.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+class KthLargest {
+public:
+    int k;
+    vector<int> nums;
+    KthLargest(int k, vector<int>& nums) {
+        this->nums=nums;
+        this->k = k;
+        sort(this->nums.begin(),this->nums.end(), greater<int>());
+    }
+    
+    int add(int val) {
+        int size = nums.size();
+        if(size==0)
+        {
+            nums.push_back(val);
+            return val;
+        }
+        
+        for(int i=0;i<size;i++)
+        {
+            if(val>nums[i])
+            {
+                nums.emplace(nums.begin()+i,val);
+                break;
+            }
+        }
+        if(nums.size()==size)
+            nums.push_back(val);
+        return nums[k-1];
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
