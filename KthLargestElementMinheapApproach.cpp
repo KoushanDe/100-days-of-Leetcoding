1be4166421ed015286a9b7f5e1d48f9b3e939476
KoushanDe <koushanriki007@gmail.com>
2022-04-08 23:08:40 +0530

    Day 11

---
+++ KthLargestElementMinheapApproach.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+class KthLargest {
+public:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> heap;
+    KthLargest(int k, vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            heap.push(nums[i]);
+            if(heap.size()>k)
+                heap.pop();
+        }
+        this->k = k;
+    }
+    
+    int add(int val) {
+        int size = heap.size();
+        if(size==0)
+        {
+            heap.push(val);
+            return val;
+        }
+        heap.push(val);
+        if(heap.size()>k)
+            heap.pop();
+        
+        return heap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
