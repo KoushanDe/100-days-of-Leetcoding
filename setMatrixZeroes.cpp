f018490553b5c3d6a9abf8a98d2613ebf9203db5
KoushanDe <koushanriki007@gmail.com>
2022-05-02 22:18:25 +0530

    Day 34

---
+++ setMatrixZeroes.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0=1;
+        int row = matrix.size()-1,col = matrix[0].size()-1;
+        
+        for(int i=0;i<matrix.size();i++)
+        {   
+            if(matrix[i][0]==0)
+                col0=0;
+            for(int j=1;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=row;i>=0;i--)
+        {
+            for(int j=col;j>=1;j--)
+            {   
+                if(matrix[i][0]==0||matrix[0][j]==0)
+                    matrix[i][j]=0;
+            }
+            if(col0==0)
+                matrix[i][0]=0;
+        }
+    }
+};
