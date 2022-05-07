abe88b7a0bf042ae11eb8e7596ced885718a7f7d
KoushanDe <koushanriki007@gmail.com>
2022-05-08 02:21:49 +0530

    Day 39

---
+++ stockSpanProblemGFG.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        stack<long long> st;
+        vector<long long> ans;
+        int i=n-1;
+        while(i>=0)
+        {
+            if(st.empty())
+            ans.push_back(-1);
+            else
+            {
+                if(st.top()>arr[i])
+                ans.push_back(st.top());
+                else
+                {
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(arr[i]);
+            i--;
+        }
+        reverse(ans.begin(),ans.end());
+     return ans;   
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}  // } Driver Code Ends
