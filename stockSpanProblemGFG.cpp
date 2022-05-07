b2839e9a1f37818588db8b7f02ba6431990d2f2d
KoushanDe <koushanriki007@gmail.com>
2022-05-08 04:09:35 +0530

    Day 39

--- stockSpanProblemGFG.cpp
+++ stockSpanProblemGFG.cpp
@@ -4,56 +4,71 @@ using namespace std;
 
 
  // } Driver Code Ends
+
+
 class Solution
 {
     public:
-    //Function to find the next greater element for each element of the array.
-    vector<long long> nextLargerElement(vector<long long> arr, int n){
-        // Your code here
-        stack<long long> st;
-        vector<long long> ans;
-        int i=n-1;
-        while(i>=0)
-        {
-            if(st.empty())
-            ans.push_back(-1);
-            else
-            {
-                if(st.top()>arr[i])
-                ans.push_back(st.top());
-                else
-                {
-                    st.pop();
-                    continue;
-                }
-            }
-            st.push(arr[i]);
-            i--;
-        }
-        reverse(ans.begin(),ans.end());
-     return ans;   
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       stack<int> st;
+       vector<int> index;
+       vector<int> ans;
+       int i = 0;
+       while(i<n)
+       {
+           if(st.empty())
+           index.push_back(-1);
+           else
+           {
+               if(price[st.top()]>price[i])
+               {
+                   index.push_back(st.top());
+               }
+               else
+               {
+                   st.pop();
+                   continue;
+               }
+           }
+           st.push(i);
+           i++;
+       }
+       for(int j=0;j<index.size();j++)
+       {
+           ans.push_back(j-index[j]);
+       }
+       return ans;
     }
 };
+
+
 
 // { Driver Code Starts.
 
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        
-        int n;
-        cin>>n;
-        vector<long long> arr(n);
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        
-        Solution obj;
-        vector <long long> res = obj.nextLargerElement(arr, n);
-        for (long long i : res) cout << i << " ";
-        cout<<endl;
-    }
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
 	return 0;
-}  // } Driver Code Ends
+}
+  // } Driver Code Ends
