a0c38c43e827e164edda0fc62cd304ff9a8fae35
KoushanDe <koushanriki007@gmail.com>
2022-05-28 20:37:35 +0530

    Day 59

---
+++ missingNumber.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum=0;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        
+        return n*(n+1)/2-sum;
+    }
+};
