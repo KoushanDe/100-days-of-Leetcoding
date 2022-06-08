19a099a72f04583ea4cc870d65f02d9683bafbf5
KoushanDe <koushanriki007@gmail.com>
2022-06-08 22:33:07 +0530

    Day 69

---
+++ rempvePalindromicSubsequences.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        string temp(s);
+        reverse(temp.begin(),temp.end());
+        if(s==temp)return 1;
+        return 2;
+    }
+};
