3c9396f5b550a538e8b74e6bc3f38ea09ef2875b
KoushanDe <koushanriki007@gmail.com>
2022-06-09 19:09:27 +0530

    Day 70

--- maxPathSuminaMatrixCS.cpp
+++ maxPathSuminaMatrixCS.cpp
@@ -1,4 +1,4 @@
-/**MEMOISATION**/
+/**MEMOISATION**
 #include<bits/stdc++.h>
 int func(vector<vector<int>>& matrix,int i, int j,int n, int m,vector<vector<int>>& dp)
 {
@@ -18,6 +18,35 @@ int getMaxPathSum(vector<vector<int>> &matrix)
     for(int j=0;j<matrix[0].size();j++)
     {
         maxm = max(maxm,func(matrix,0,j,matrix.size(),matrix[0].size(),dp));
+    }
+   return maxm;
+}
+*/
+/**TABULATION**/
+#include<bits/stdc++.h>
+
+int getMaxPathSum(vector<vector<int>> &matrix)
+{    
+    int maxm = INT_MIN;
+    int n = matrix.size(),m=matrix[0].size();
+    vector<vector<int>> dp(n,vector<int>(m,INT_MIN));
+    for(int i=n-1;i>=0;i--)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(i==n-1)dp[i][j] = matrix[i][j];
+            else
+            {    
+                int down = dp[i+1][j];
+                int dl = j==0? INT_MIN : dp[i+1][j-1];
+                int dr = j==m-1? INT_MIN : dp[i+1][j+1];
+                dp[i][j] = matrix[i][j] + max(down,max(dl,dr));
+            }
+        }
+    }
+    for(int j=0;j<m;j++)
+    {
+        maxm = max(maxm,dp[0][j]);
     }
    return maxm;
 }
