8152577037d72460fce46b3a83bdeba349f0335e
KoushanDe <koushanriki007@gmail.com>
2022-06-13 21:47:45 +0530

    Day 74

---
+++ longestCommonSubstring.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lcstr(string &str1, string &str2){
+	int n = str1.size();
+    int m = str2.size();
+    
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0)); //shifting of index
+    int maxlen=INT_MIN;
+    
+    for(int i=0;i<=n;i++)
+        dp[i][0]=0;
+    for(int j=0;j<=m;j++)
+        dp[0][j]=0;
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if(str1[i-1]==str2[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j]=0;
+            maxlen = max(maxlen,dp[i][j]);
+        }
+    }
+    
+    return maxlen;
+}
