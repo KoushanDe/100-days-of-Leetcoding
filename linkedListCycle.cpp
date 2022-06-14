ddb95f9a6c850b2353c1ef0e7dde480bdd5dbf3e
KoushanDe <koushanriki007@gmail.com>
2022-06-14 14:14:48 +0530

    Day 75

---
+++ linkedListCycle.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(true)
+        {
+            if(fast==NULL||fast->next==NULL)
+                return false;
+            slow=slow->next;
+            fast = fast->next->next;
+            if(slow==fast)
+                return true;
+        }
+    }
+};
