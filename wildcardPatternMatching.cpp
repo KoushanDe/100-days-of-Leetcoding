9a3233a23146ec296bd47b77f30b3468c01c0091
KoushanDe <koushanriki007@gmail.com>
2022-06-15 02:29:36 +0530

    Day 76

--- wildcardPatternMatching.cpp
+++ wildcardPatternMatching.cpp
@@ -51,3 +51,29 @@ bool wildcardMatching(string pattern, string text)
     }
     return dp[n][m];
 }
+/**SPACE OPTIMISATION**
+bool wildcardMatching(string pattern, string text)
+{
+    int n = pattern.size();
+    int m = text.size();
+    //vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    vector<bool> prev(m+1,false), curr(m+1,false);
+    prev[0] = true;
+    for(int j=1;j<=m;j++) prev[j] = false;
+    
+    for(int i=1;i<=n;i++)
+    {    
+        curr[0] = prev[0] && pattern[i-1]=='*';
+        for(int j=1;j<=m;j++)
+        {
+            if(pattern[i-1]==text[j-1]||pattern[i-1]=='?')
+                curr[j]=prev[j-1];
+            else if(pattern[i-1]=='*')
+                curr[j]=prev[j]||curr[j-1];
+            else
+                curr[j]=false;
+        }
+        prev = curr;
+    }
+    return prev[m];
+}*/
