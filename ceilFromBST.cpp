c2eb2aacae9dca7be27488491d5e643b9b4808cd
KoushanDe <koushanriki007@gmail.com>
2022-06-27 02:44:50 +0530

    Day 88

---
+++ ceilFromBST.cpp
@@ -0,0 +1,51 @@
+/************************************************************
+
+    Following is the TreeNode class structure:
+
+    class BinaryTreeNode {
+    public:
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+        
+        BinaryTreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+        
+        ~BinaryTreeNode() {
+            if (left) {
+              delete left;
+            }
+            if (right) {
+              delete right;
+            }
+        }
+    };
+
+************************************************************/
+void helper(BinaryTreeNode<int>* node, int x, int& minceil)
+{
+    if(node==NULL) return;
+    
+    if(x==node->data)
+    {
+        minceil=x;
+        return;
+    }
+    else if(x>node->data)
+        helper(node->right,x,minceil);
+    else
+    {
+        minceil = min(minceil,node->data);
+        helper(node->left,x,minceil);
+    }
+}
+
+int findCeil(BinaryTreeNode<int> *node, int x){
+    // Write your code here.
+    int minceil=INT_MAX;
+    helper(node,x,minceil);
+    return minceil==INT_MAX?-1:minceil;
+}
--- flattenBinaryTreetoLinkedList.cpp
+++ flattenBinaryTreetoLinkedList.cpp
@@ -23,3 +23,28 @@ public:
         prev=root;
     }
 };
+
+/**ITERATIVE**
+ * class Solution {
+public:
+    
+    void flatten(TreeNode* root) {
+        if(root==NULL)return;
+        
+        stack<TreeNode*> st;
+        
+        st.push(root);
+        
+        while(!st.empty())
+        {
+            TreeNode* curr = st.top();
+            st.pop();
+            
+            if(curr->right) st.push(curr->right);
+            if(curr->left) st.push(curr->left);
+            
+            if(!st.empty())curr->right=st.top();
+            curr->left=NULL;
+        }
+    }
+};*/
