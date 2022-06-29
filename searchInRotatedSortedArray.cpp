5c4f5f2b29cc7730bab71567ca1310ac5595cdc6
KoushanDe <koushanriki007@gmail.com>
2022-06-30 01:39:40 +0530

    Day 91

--- searchInRotatedSortedArray.cpp
+++ searchInRotatedSortedArray.cpp
@@ -2,56 +2,90 @@
 using namespace std;
 class Solution {
 public:
-    
-    int findMin(vector<int>& nums) {
+    int search(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size()-1;
         
-        int n = nums.size();
-        
-        if(nums[0]<=nums[n-1])
-            return 0;
-        
-        int low=0;
-        int high = n-1;
-        
-        while(low<=high)
+        while(lo<=hi)
         {
-            int mid = low + (high-low)/2;
-            
-            if(nums[mid]<nums[(n+mid-1)%n]&&nums[mid]<nums[(mid+1)%n])
-                return mid;
-            else if(nums[mid]>=nums[0])
-                low=mid+1;
-            else
-                high=mid-1;
-        }
-        return 0;
-    }
-    
-    int BS(vector<int>&nums,int target,int low,int high)
-    {
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
+            int mid = (lo+hi)/2;
             
             if(nums[mid]==target)
                 return mid;
-            else if(nums[mid]<target)
-                low = mid+1;
+            
+            else if(nums[mid]>=nums[lo])
+            {
+                if(nums[lo]<=target&&target<=nums[mid])
+                    hi=mid-1;
+                else
+                    lo=mid+1;
+            }
             else
-                high=mid-1;
+            {
+                if(nums[mid]<=target&&target<=nums[hi])
+                    lo=mid+1;
+                else
+                    hi=mid-1;
+            }
         }
+        
         return -1;
     }
-    
-    int search(vector<int>& nums, int target) {
-        int index = findMin(nums);
-        int a,b;
-        
-        a=BS(nums,target,0,index-1);
-        b=BS(nums,target,index,nums.size()-1);   
-        
-        
-        
-        return max(a,b);
-    }
 };
+
+//Most probably aditya verma solution
+// class Solution {
+// public:
+    
+//     int findMin(vector<int>& nums) {
+        
+//         int n = nums.size();
+        
+//         if(nums[0]<=nums[n-1])
+//             return 0;
+        
+//         int low=0;
+//         int high = n-1;
+        
+//         while(low<=high)
+//         {
+//             int mid = low + (high-low)/2;
+            
+//             if(nums[mid]<nums[(n+mid-1)%n]&&nums[mid]<nums[(mid+1)%n])
+//                 return mid;
+//             else if(nums[mid]>=nums[0])
+//                 low=mid+1;
+//             else
+//                 high=mid-1;
+//         }
+//         return 0;
+//     }
+    
+//     int BS(vector<int>&nums,int target,int low,int high)
+//     {
+//         while(low<=high)
+//         {
+//             int mid=low+(high-low)/2;
+            
+//             if(nums[mid]==target)
+//                 return mid;
+//             else if(nums[mid]<target)
+//                 low = mid+1;
+//             else
+//                 high=mid-1;
+//         }
+//         return -1;
+//     }
+    
+//     int search(vector<int>& nums, int target) {
+//         int index = findMin(nums);
+//         int a,b;
+        
+//         a=BS(nums,target,0,index-1);
+//         b=BS(nums,target,index,nums.size()-1);   
+        
+        
+        
+//         return max(a,b);
+//     }
+// };
--- singleElementInASortedArray.cpp
+++ singleElementInASortedArray.cpp
@@ -12,20 +12,28 @@ public:
         {
             int mid = (lo+hi)/2;
             
-            if(mid%2==1)
+            if(mid%2==1) //odd
             {
                 if(nums[mid]==nums[mid-1])
                     lo=mid+1;
                 else
                     hi=mid-1;
             }
-            else
+            else //even
             {
                 if(nums[mid]==nums[mid+1])
                     lo=mid+1;
                 else
                     hi=mid-1;
             }
+
+            //here there is a cool trick to avoid writing both even and odd conditions
+            /*
+            if(nums[mid]==nums[mid^1]) //odd number xor 1 = even number before it & even number xor 1 = odd number after it  11^1 = 10, 10^1=11
+                lo=mid+1;
+            else
+                hi=mid-1;
+            */
         }
         
         return nums[lo];
