d565e02647f4ac314fafbdf39433e9a417911587
KoushanDe <koushanriki007@gmail.com>
2022-07-04 22:35:05 +0530

    Day 95

---
+++ implementStackUsingOneQueue.cpp
@@ -0,0 +1,41 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int size = q.size();
+        
+        for(int i=1;i<=size-1;i++)
+        {
+            int temp = q.front();
+            q.pop();
+            q.push(temp);
+        }
+    }
+    
+    int pop() {
+        int temp = q.front();
+        q.pop();
+        return temp;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
