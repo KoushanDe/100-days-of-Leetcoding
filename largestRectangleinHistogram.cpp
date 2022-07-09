22a9d908cbdef117084afc760ded4d24ab6c4d39
KoushanDe <koushanriki007@gmail.com>
2022-07-10 04:52:39 +0530

    Day 101

--- largestRectangleinHistogram.cpp
+++ largestRectangleinHistogram.cpp
@@ -66,3 +66,33 @@ public:
         return maxarea;
     }
 };
+
+//OPTIMAL
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        
+        int maxarea=0;
+        int n = heights.size();
+        
+        for(int i=0;i<=n;i++)
+        {
+            while(!st.empty()&&(i==n||heights[st.top()]>=heights[i]))
+            {
+                int height = heights[st.top()];
+                st.pop();
+                
+                int width;
+                if(st.empty()) width=i;
+                else width = i-st.top()-1;
+                
+                maxarea = max(maxarea,height*width);
+                
+            }
+            st.push(i);
+        }
+        
+        return maxarea;
+    }
+};
