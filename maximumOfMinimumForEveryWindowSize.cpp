f6f5a4acd6d808d6aa2be91c8fb9ea5f80be71bf
KoushanDe <koushanriki007@gmail.com>
2022-07-11 21:58:40 +0530

    Day 102

--- maximumOfMinimumForEveryWindowSize.cpp
+++ maximumOfMinimumForEveryWindowSize.cpp
@@ -32,3 +32,76 @@ vector<int> maxMinWindow(vector<int> a, int n) {
     
     return maxminwindows;
 }*/
+#include<bits/stdc++.h>
+vector<int> maxMinWindow(vector<int> a, int n) {
+    vector<int> maxminwindows(n,INT_MIN);
+    
+    vector<int> lse;
+    vector<int> rse;
+    
+    stack<int> st;
+    
+    for(int i=0;i<n;)
+    {
+        if(st.empty())
+            lse.push_back(-1);
+        else
+        {
+            if(a[i]<=a[st.top()])
+            {
+                st.pop();
+                continue;
+            }
+            else
+                lse.push_back(st.top());
+        }
+        st.push(i++);
+    }
+    
+    while(!st.empty())
+        st.pop();
+    
+    for(int i=n-1;i>=0;)
+    {
+        if(st.empty())
+            rse.push_back(n);
+        else
+        {
+            if(a[i]<=a[st.top()])
+            {
+                st.pop();
+                continue;
+            }
+            else
+                rse.push_back(st.top());
+        }
+        st.push(i--);
+    }
+    reverse(rse.begin(),rse.end());
+    
+//     for(auto num:rse)
+//         cout<<num<<" ";
+//     cout<<endl;
+//     for(auto num:lse)
+//         cout<<num<<" ";
+//     cout<<endl;
+    
+    for(int i=0;i<n;i++)
+    {
+        int winsize = rse[i] - lse[i] -1;
+        
+        if(maxminwindows[winsize-1]<a[i])
+            maxminwindows[winsize-1]=a[i];
+    }
+
+    int prev=INT_MIN;
+    for(int i=n-1;i>=0;i--)
+    {
+        if(maxminwindows[i]>prev)
+            prev=maxminwindows[i];
+        else
+            maxminwindows[i]=prev;
+    }
+    
+    return maxminwindows;
+}
