e0a1c56a545fa5a88493679013b3d6727067f88f
KoushanDe <koushanriki007@gmail.com>
2022-07-31 19:18:04 +0530

    Day 122

--- longestIncreasingPathinaMatrix.cpp
+++ longestIncreasingPathinaMatrix.cpp
@@ -34,3 +34,44 @@ public:
         return longestPath;
     }
 };
+
+//Another approach
+class Solution {
+public:
+    int findLPath(vector<vector<int>>& matrix, int i, int j, int m, int n,vector<vector<int>>& dp)
+    {   
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int longestPath=1;
+        
+        if(i-1>=0&&matrix[i-1][j]>matrix[i][j])
+            longestPath = max(longestPath,1+findLPath(matrix,i-1,j,m,n,dp));
+        if(i+1<m&&matrix[i+1][j]>matrix[i][j])
+            longestPath = max(longestPath,1+findLPath(matrix,i+1,j,m,n,dp));
+        if(j-1>=0&&matrix[i][j-1]>matrix[i][j])
+            longestPath = max(longestPath,1+findLPath(matrix,i,j-1,m,n,dp));
+        if(j+1<n&&matrix[i][j+1]>matrix[i][j])
+            longestPath = max(longestPath,1+findLPath(matrix,i,j+1,m,n,dp));
+        
+        return dp[i][j]=longestPath;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+        int longestPath = 0;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int pathsize = findLPath(matrix,i,j,m,n,dp);
+                longestPath = max(longestPath,pathsize);
+            }
+        }
+        
+        return longestPath;
+    }
+};
